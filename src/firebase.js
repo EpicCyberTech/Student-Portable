8c066c0a843a79bb3b56d5b2099543d0fb89f2f0
EpicCyberTech <epiccybertechect@gmail.com>
2025-10-17 13:22:58 +0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,1 +1,13 @@
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDFD0ilbHx70I-1Z1vtMVmnx6mLt30Pf44",
+  authDomain: "student-portal-6c104.firebaseapp.com",
+  projectId: "student-portal-6c104",
+  storageBucket: "student-portal-6c104.firebasestorage.app",
+  messagingSenderId: "386568197727",
+  appId: "1:386568197727:web:4007afb1df2df05ce52dea",
+  measurementId: "G-VR4YZLTG72"
+};
+
+export default firebaseConfig;
